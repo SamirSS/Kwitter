31ec047dfc4bc03e057080e69af39563f06a3608
Andrew <16abrander@my.fenwickfriars.com>
2015-08-05 18:47:21 +0000

    the database update

--- models/tweet.rb
+++ models/tweet.rb
@@ -1,16 +1,16 @@
  require_relative 'user.rb'
-class Tweet
+class Tweet < ActiveRecord::Base
  
-  attr_accessor :user, :message, :to_user
+#   attr_accessor :user, :message, :to_user
   
-  def initialize(user, message, to_user)
-    @user = user
-    @message = message.to_s
-    @to_user = to_user
-  end
+#   def initialize(user, message, to_user)
+#     @user = user
+#     @message = message.to_s
+#     @to_user = to_user
+#   end
   
-  def display_tweet
-    return "#{@user} sent a tweet to #{@to_user.username} <br />#{@message} <br />"
-  end
+#   def display_tweet
+#     return "#{@user} sent a tweet to #{@to_user.username} <br />#{@message} <br />"
+#   end
   
 end
