090e79fcdbaf1cdbc48ade3c91dd7334bf764f5a
Andrew <16abrander@my.fenwickfriars.com>
2015-08-05 15:56:29 +0000

    HAI DIS IS ANDREW

--- application_controller.rb
+++ application_controller.rb
@@ -1,4 +1,5 @@
 require 'bundler'
+require 'pry'
 Bundler.require
 require_relative 'models/tweet.rb'
 require_relative 'models/user.rb'
@@ -6,16 +7,25 @@ require_relative 'models/user.rb'
 class ApplicationController < Sinatra::Base
   
   get '/' do 
-    @user = User.new("Kanye", 219000, 2)
-    @user2 = User.new("Kim", 4, 3000)
+  @user = User.new("Kanye",219000,2)
+  @user2 = User.new("Kim",4,3000)
     erb :index
   end
-    
+  
   post "/send_tweet" do
-    @user = User.new("Kanye", 219000, 2)
-    @user2 = User.new("Kim", 4, 3000)
-    @user.send_tweet(params[:send_tweet], @user2)
+     @user = User.new("Kanye", 219000, 2)
+     @user2 = User.new("Kim", 4, 3000)
+      
+    @user.send_tweet(params[:send], @user2)
+ 
     erb :index
   end
-
+  get '/profile/:name'  do
+    @hi = User.new(params[:name], 153, 374)
+  @hi2 = User.new("Tim Cook", 40000000, 3)
+  @hi.send_tweet("Hi",@hi2)
+   @hi.send_tweet("params[:send]", @hi2)
+   @hi.send_tweet("Thanks, Tim", @hi2)
+  erb :profile
+end
 end
--- models/tweet.rb
+++ models/tweet.rb
@@ -1,15 +1,16 @@
+ require_relative 'user.rb'
 class Tweet
-  
+ 
   attr_accessor :user, :message, :to_user
   
   def initialize(user, message, to_user)
     @user = user
-    @message = message
+    @message = message.to_s
     @to_user = to_user
   end
   
   def display_tweet
-    return "#{@user} sent a tweet to #{@to_user}\n#{@message}"
+    return "#{@user} sent a tweet to #{@to_user.username} <br />#{@message} <br />"
   end
   
 end
